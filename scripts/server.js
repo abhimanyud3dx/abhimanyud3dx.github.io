5a354ac21049ac1fd3277ec94bccaa72b9dbf290
abhimanyu_mtx <abhimanyu.tanwar@mtxb2b.com>
2023-02-09 20:12:56 +0530

    initial build

--- scripts/server.js
+++ scripts/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const siteConfig = require('../config/blog.js');
 
-const app = express(); //Test 1
+const app = express(); //Test 3
 
 let directives = {
     defaultSrc: ["'self'"],
